3b373d990eb00e7e3e3e4c73f3e5fc44646b10aa
otills <oliver.tills@plymouth.ac.uk>
2025-03-18 18:49:58 +0000

    Deploying to gh-pages from @ phenomyx/phenomyx@d2b20469318620d64ccc552f21a51973e0715649 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,14 +511,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=o7gR40kAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
